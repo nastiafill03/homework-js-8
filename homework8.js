aa14847dab301c50f12c2cfee1937790832c7963
nastiafill03 <149972381+nastiafill03@users.noreply.github.com>
2026-02-05 15:57:58 +0100

    Add files via upload

---
+++ homework8.js
@@ -0,0 +1,7 @@
+// Домашнє завдання
+// -----1-----
+// Напишіть код, щоб вибрати елемент з атрибутом data-widget-name з документа
+// та прочитати його значення.
+// const element = document.querySelector("[data-widget-name]");
+// const value = element.getAttribute("data-widget-name");
+// console.log(value);
